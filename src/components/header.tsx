d01a12ca50d1f1f1facb75c78eac62032050ebfd
Hafiz Absar Ali <absarali701@gmail.com>
2023-06-23 02:36:44 +0500

    ShadCn integrated

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "@/components/ui/button"
+
+const Header = () => {
+  return (
+    <div className='flex'>
+    <div></div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default Header;
